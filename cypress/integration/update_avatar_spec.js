cde55889e67af27cd691e90d05ed0639fdd1ab11
Daniel Cueva <cuevadaniel1@gmail.com>
2022-07-28 09:56:13 -0700

    include spec for avater upload

---
+++ cypress/integration/update_avatar_spec.js
@@ -0,0 +1,20 @@
+describe('Upload Avatar', () => {
+  before(() => {
+    const qaemail = 'cuevadaniel1+leadqahw@gmail.com'
+    const qapassword = 'Password1'
+
+    cy.visitThis('https://www.getcloudapp.com/login')
+    cy.signIn(qaemail, qapassword)
+
+    cy.visitThis('https://share.getcloudapp.com/organizations/WjFZkJAl/account/settings#profile')
+})
+    
+  it('lets user update avatar', () => {
+    const avatar = 'Picture.jpg'
+
+    cy.get('#user_avatar').attachFile(avatar)
+    cy.get('[data-testid="onboarding-submit-about-you-form"]')
+        .click()
+    cy.contains('Account updated successfully')
+  })
+})
