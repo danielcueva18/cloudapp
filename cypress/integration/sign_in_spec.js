5f9e8fb617dd14dcf6ad0da34865a5e425330c4a
Daniel Cueva <cuevadaniel1@gmail.com>
2022-07-28 09:22:51 -0700

    use can sign out

--- cypress/integration/sign_in_spec.js
+++ cypress/integration/sign_in_spec.js
@@ -1,22 +1,30 @@
 describe('Sign In Form', () => {
-    before(() => {
-      cy.visitThis('https://www.getcloudapp.com/login')
-    })
-  
-    it('lets user sign in', () => {
-      cy.get('[data-testid="regular-login-email"]')
-        .type('cuevadaniel1+leadqahw@gmail.com')
-  
-      cy.get('#password')
-        .type('Password1')
-  
-      cy.get('[data-testid="regular-login-password"]')
-        .click()
-      cy.get('[data-testid="regular-login-submit"]')
-        .click()
+  beforeEach(() => {
+    const qaemail = 'cuevadaniel1+leadqahw@gmail.com'
+    const qapassword = 'Password1'
 
+    cy.visitThis('https://www.getcloudapp.com/login')
+    cy.signIn(qaemail, qapassword)
+  })
+
+  context('Test log in feature', () => {        
+    it('lets user sign in', () => {
       cy.contains('Welcome back')
       cy.url().should('include', '/dashboard')
     })
   })
+
+  context('Test log out feature', () => {
+    it('lets user log out', () => {
+      cy.get('#main-menu')
+        .click()
+        
+      cy.get('[data-testid="dropdown-link-sign_out"]')
+        .click()
+
+      cy.contains('Welcome back')
+      cy.url().should('include', '/login') 
+    })
+  })
+})
   
