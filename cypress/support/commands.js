a4bfce000bae74fe8eebea4bf7c516405efec14d
Daniel Cueva <cuevadaniel1@gmail.com>
2022-07-27 19:58:44 -0700

    reusable command to visit url

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('visitThis', (url, options) => {
+    options = options || {}
+    
+    cy.visit(url)
+})
