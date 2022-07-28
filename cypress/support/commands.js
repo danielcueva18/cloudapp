a30bfb6c382d39cb875f16d4905d73628ae6454b
Daniel Cueva <cuevadaniel1@gmail.com>
2022-07-28 09:22:33 -0700

    make login into a command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -29,3 +29,16 @@ Cypress.Commands.add('visitThis', (url, options) => {
     
     cy.visit(url)
 })
+
+Cypress.Commands.add('signIn', (email, password) => {
+  cy.get('[data-testid="regular-login-email"]')
+    .type(email)
+
+  cy.get('#password')
+    .type(password)
+
+  cy.get('[data-testid="regular-login-password"]')
+    .click()
+  cy.get('[data-testid="regular-login-submit"]')
+    .click()
+})
